e8d86960251f619fc0c957f5406e647380d9cf5a
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-27 01:25:04 +0800

    bug fix

--- alpine-start.sh
+++ alpine-start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 #sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+
 apk update && apk add docker ttyd nginx python3 curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip install flask
 mkdir /run/nginx/ && touch /run/nginx/nginx.pid 
+cd `dirname $0`
 cp default.conf /etc/nginx/conf.d/default.conf
 nginx
 python3 app.py
