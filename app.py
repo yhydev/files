2a8f09e5f12628403658843bb05c5e072c6e45b4
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-27 01:06:46 +0800

    fix bug

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os, re, time, socket, sys
 app = Flask(__name__)
 
 SO_BINDTODEVICE=25
-BASE_PATH = os.path.dirname(sys.argv[0])
+BASE_DIRNAME = os.path.dirname(sys.argv[0])
+INDEX_PATHNAME = os.path.join(BASE_DIRNAME, "index.html")
 
 def getFreePort(iface=None):
     s = socket.socket()
@@ -27,10 +28,10 @@ def createLinux():
 
         logfilename = "/tmp/share-linux-%s.log" % str(1000 * time.time())
         port = getFreePort()
-        cmd = "nohup  ttyd -I %sindex.html -p %d docker run -it ubuntu bash > %s 2>&1 &" % (BASE_PATH or BASE_PATH + "/", port, logfilename)
+        cmd = "nohup  ttyd -I %s -p %d docker run -it ubuntu bash > %s 2>&1 &" % (INDEX_PATHNAME, port, logfilename)
         os.system(cmd)
         return jsonify({"port": port})
 
 
 if __name__ == '__main__':
-       app.run(host = "0.0.0.0")
+        app.run(host = "0.0.0.0")
