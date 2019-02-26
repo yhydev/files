979a49635cf8be95efda28bab7758b2f94bd64f8
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-27 00:17:08 +0800

    update alpine-start.sh

--- alpine-start.sh
+++ alpine-start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
-apk update && apk add docker ttyd nginx python3
+apk update && apk add docker ttyd nginx python3 curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+python3 get-pip.py
 pip install flask
 mkdir /run/nginx/ && touch /run/nginx/nginx.pid 
 cp default.conf /etc/nginx/conf.d/default.conf
-python3 app.py
 nginx
+python3 app.py
