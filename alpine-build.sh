8a74c87ac1855ca6a3ea4919b04263e54ab79924
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-27 18:16:50 +0800

    update

--- alpine-build.sh
+++ alpine-build.sh
@@ -3,10 +3,20 @@
 #sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 
 apk update && apk add docker ttyd nginx python3 curl
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+
+pipFileName=/tmp/get-pip.py
+
+if [! -e $pipFileName];then
+curl https://bootstrap.pypa.io/get-pip.py -o $pipFileName
+python3 $pipFileName
+fi
+
 pip install flask
-mkdir /run/nginx/ && touch /run/nginx/nginx.pid 
+
+if [! -e "/run/nginx/nginx.pid"];then
+	mkdir /run/nginx/ && touch /run/nginx/nginx.pid 
+fi
+
 cd `dirname $0`
 cp default.conf /etc/nginx/conf.d/default.conf
 cp index.html /var/lib/nginx/html/index.html
