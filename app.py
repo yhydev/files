52cbf386fbf6a3065e1ec6a6e7286fd1872c8788
yhydev <31916483+yhydev@users.noreply.github.com>
2019-02-28 04:54:18 +0800

    fix bug

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 #coding=utf-8
 
 from flask import Flask, jsonify, url_for, render_template, request
-import os, re, time, socket, sys, docker
+import os, re, time, socket, sys, docker, logging
+
+logging.basicConfig(level = logging.INFO)
 
 app = Flask(__name__)
 
@@ -10,6 +12,9 @@ SO_BINDTODEVICE=25
 
 #文件路径
 BASE_DIRNAME = os.path.dirname(sys.argv[0])
+
+#日志文件目录
+LOG_DIR = "/tmp/share-linux"
 
 #终端文件路径
 INDEX_PATHNAME = os.path.join(BASE_DIRNAME, "terminal.html")
@@ -19,6 +24,23 @@ INDEX_PATHNAME = os.path.join(BASE_DIRNAME, "terminal.html")
 
 #docker 接口
 client = docker.from_env()
+
+def clean():
+    logFileNames = os.listdir(LOG_DIR)
+    try:
+        for fileName in logFileNames:
+            client.containers.get(fileName).remove(force = True)
+            os.remove(os.path.join(LOG_DIR, fileName))
+    except Exception as e:
+        logging.exception(e)
+
+
+
+def cleanThreading():
+     while True:
+        clean()
+        time.sleep(5)
+
 
 
 def getFreePort(iface=None):
@@ -41,15 +63,14 @@ def run():
         Description:
         运行一个容器
         """
-        logfilename = "/tmp/share-linux-%s.log" % str(1000 * time.time())
         image = "ubuntu"
         ret = None
         status = 200
         try:
             port = getFreePort()
-            hostName = "share-linux-%d" % port
             container = client.containers.run(image, "sleep 2h", name = hostName, detach = True, remove = True)
-            cmd = "nohup  ttyd -d 0 -I %s -p %d docker exec -it %s bash > %s 2>&1 &" % (INDEX_PATHNAME, port, hostName, logfilename)
+            logFileName = os.path.join(LOG_DIR, container.name)
+            cmd = "nohup  ttyd -I %s -p %d docker exec -it %s bash > %s 2>&1 &" % (INDEX_PATHNAME, port, container.name, logFileName)
             os.system(cmd)
             ret = jsonify({"port": port})
         except Exception as e:
@@ -58,6 +79,10 @@ def run():
         
         return ret, status
 
+
+
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -65,4 +90,9 @@ def index():
 
 
 if __name__ == '__main__':
+        if !os.path.exists(LOG_DIR):
+            os.path.mkdir(LOG_DIR)
+
+        threading.Thread(cleanThreading).start()
+
         app.run(host = "0.0.0.0")
